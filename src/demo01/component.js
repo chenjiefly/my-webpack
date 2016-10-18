4f615bc872e50bc26510a71d830d229c80011a2d
cj85996 <cj85996@alibaba-inc.com>
2016-10-18 21:37:52 +0800

    完成基本的修改后自动刷新，但是还需要解决开发模式和生产模式下html文件中的目标js

--- src/demo01/component.js
+++ src/demo01/component.js
@@ -1,3 +1,10 @@
+import $ from 'jquery'
+
+let showMyName = (name) => {
+  $('h1').text(`Hello world! My name is ${ name }!`)
+}
+
 module.exports = function(){
-    alert('component6');
+  showMyName('chenjie')
+  alert('component')
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 // var fs = require('fs')
-var path = require('path')
+// var path = require('path')
 
 var webpack = require('webpack')
 // var extractTextPlugin = require('extract-text-webpack-plugin')  // 独立打包样式文件
 
-var ROOT = __dirname
-var srcDir = path.resolve(ROOT, 'src')
-var distDir = path.resolve(ROOT, 'dist')
+// var ROOT = __dirname
+// var srcDir = path.resolve(ROOT, 'src')
 
 // 创建入口文件集合对象
 function makeEntry() {
@@ -20,89 +19,73 @@ module.exports = {
   output: {
     path: './dist/',
     filename: '[name]/bundle.js'
+    // publicPath: "/assets/"
+  },
+
+  externals: {
+    // 'jquery': 'jQuery'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url',
+        query: {
+          limit: 8192
+        }
+      }
+    ],
+    noParse: [
+      // 不希望webpack处理的模块的正则表达式
+    ]
+  },
+  // resolve: {
+  //   root: srcDir,  //绝对路径
+  //   extensions: ['', '.js', '.json', '.scss'],
+  //   alias: {
+  //     // 模块映射表
+  //   }
+  // },
+
+  // babel使用的插件
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
   },
 
   plugins: [
     // 热替换插件，不需要浏览器自动刷新就可以更新页面内容
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+
+    // 抽取公共模块
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: 'common',  // chunkname
+    //   filename: 'js/common.js',  // 输出文件名
+    //   minChunks: 2,  // 至少被2个以上的模块引用才会被抽取为公共模块,
+    //   chunks: makeEntry()
+    // }),
+
+    // // 单独提取样式文件
+    // new extractTextPlugin('[name].css')
   ],
 
+  // webpack-dev-server 配置
   devServer: {
-    // publicPath: "/static/",
+    // publicPath: "/assets/",
     stats: { colors: true },
     port: 8080,
     // contentBase: './dist',
+
+    // 本地js修改后，浏览器自动刷新页面
     inline: true,
+
+    // 热启动，需要配合HotModuleReplacementPlugin插件使用
     hot: true
   }
 }
-
-// module.exports = {
-//   entry: makeEntry(),
-
-//   output: {
-//     // path: distDir,
-//     filename: 'bundle.js'
-//   },
-
-//   externals: {
-//     'jquery': 'jQuery'
-//   },
-
-//   module: {
-//     loaders: [
-//       {
-//         test: /\.js$/,
-//         loader: 'babel',
-//         exclude: /node_modules/
-//       },
-//       {
-//         test: /\.(png|jpg)$/,
-//         loader: 'url',
-//         query: {
-//           limit: 8192
-//         }
-//       }
-//     ],
-//     noParse: [
-//       // 不希望webpack处理的模块的正则表达式
-//     ]
-//   },
-//   // resolve: {
-//   //   root: srcDir,  //绝对路径
-//   //   extensions: ['', '.js', '.json', '.scss'],
-//   //   alias: {
-//   //     // 模块映射表
-//   //   }
-//   // },
-
-//   // babel使用的插件
-//   babel: {
-//     presets: ['es2015'],
-//     plugins: ['transform-runtime']
-//   },
-
-//   // webpack插件
-//   plugins: [
-//     // // 抽取公共模块
-//     // new webpack.optimize.CommonsChunkPlugin({
-//     //   name: 'common',  // chunkname
-//     //   filename: 'js/common.js',  // 输出文件名
-//     //   minChunks: 2,  // 至少被2个以上的模块引用才会被抽取为公共模块,
-//     //   chunks: makeEntry()
-//     // }),
-
-//     // // 单独提取样式文件
-//     // new extractTextPlugin('[name].css')
-//   ],
-
-//   devServer: {
-//     publicPath: "/static/",
-//     stats: { colors: true },
-//     port: 8080,
-//     contentBase: 'build',
-//     inline: true
-//   }
-// }
-
-
